a1189b9127a178d4815c04f397dd09d38c610e7d
Lucas Hardy <1124114+TheNicestGuy@users.noreply.github.com>
2020-04-21 16:19:52 -0400

    Add ScreenProfile class and set up for all known large ship blocks with screens. ScreenProfile does not yet know about small ship blocks.

--- Program.cs
+++ Program.cs
@@ -21,62 +21,130 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        // This file contains your actual script.
-        //
-        // You can either keep all your code here, or you can create separate
-        // code files to make your program easier to navigate while coding.
-        //
-        // In order to add a new utility class, right-click on your project, 
-        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
-        // category under 'Visual C# Items' on the left hand side, and select
-        // 'Utility Class' in the main area. Name it in the box below, and
-        // press OK. This utility class will be merged in with your code when
-        // deploying your final script.
-        //
-        // You can also simply create a new utility class manually, you don't
-        // have to use the template if you don't want to. Just do so the first
-        // time to see what a utility class looks like.
-        // 
-        // Go to:
-        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
-        //
-        // to learn more about ingame scripts.
+
+        #region Program / Private Members
+
+        private MyIni _theIniParser = new MyIni();
+        private MyCommandLine _theCommandParser = new MyCommandLine();
+        private readonly IDictionary<string, Action> _knownCommands
+            = new Dictionary<string, Action>(StringComparer.InvariantCultureIgnoreCase);
+
+        #endregion Program / Private Members
+
+        #region Program / Constants
+
+        private const string INI_SECTION_NAME = "TNGForemanConsole";
+        // WARNING: Do not set the main loop to Update1 (every tick). This causes weird bugs.
+        private const UpdateFrequency MAIN_LOOP_FREQUENCY = UpdateFrequency.Update10;
+        private const UpdateType MAIN_LOOP_TYPE = UpdateType.Update10;
+
+        #endregion Program / Constants
 
         public Program()
         {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script. 
-            //     
-            // The constructor is optional and can be removed if not
-            // needed.
-            // 
-            // It's recommended to set Runtime.UpdateFrequency 
-            // here, which will allow your script to run itself without a 
-            // timer block.
-        }
+            this.Runtime.UpdateFrequency = MAIN_LOOP_FREQUENCY;
+
+
+            // Load state from storage
+            if (this._theIniParser.TryParse(this.Storage))
+            {
+                // ...
+            } // if could parse Storage
+            this._theIniParser.Clear();
+
+            // TEMP: testing code
+            List<IMyTerminalBlock> allBlocks = new List<IMyTerminalBlock>();
+            this.GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(
+                allBlocks
+                , block => MyIni.HasSection(block.CustomData, INI_SECTION_NAME)
+            );
+
+            foreach (IMyTerminalBlock thisBlock in allBlocks)
+            {
+                IMyTextSurfaceProvider thisScreenGroup = thisBlock as IMyTextSurfaceProvider;
+                if (null == thisScreenGroup)
+                {
+                    Echo($"{thisBlock.CustomName} is not an IMyTextSurfaceProvider.");
+                }
+                else
+                {
+                    Echo($"{thisBlock.CustomName} provides {thisScreenGroup.SurfaceCount} text surfaces.");
+                    for (int i = 0; i < thisScreenGroup.SurfaceCount; i++)
+                    {
+                        IMyTextSurface thisScreen = thisScreenGroup.GetSurface(i);
+                        thisScreen.ContentType = ContentType.TEXT_AND_IMAGE;
+                        float thisAspectRatio = thisScreen.SurfaceSize.X / thisScreen.SurfaceSize.Y;
+                        try
+                        {
+                            ScreenProfile thisScreenProfile = ScreenProfile.GetScreenProfile(thisScreenGroup, i);
+                            thisScreen.FontSize = thisScreenProfile.BestFontSize;
+                        } catch (Exception e)
+                        {
+                            Echo(e.Message);
+                        }
+                        //thisScreen.FontSize = thisScreenGroup.GetOptimalFontSize(i, false);
+                        thisScreen.WriteText(
+                            $"{thisBlock.CustomName}[{i.ToString("f0")}]"
+                            + $" ({thisScreenGroup.GetSurface(i).Name})"
+                            + $"\n{thisScreen.SurfaceSize.ToString()}"
+                            + $" (ratio {thisAspectRatio.ToString()})"
+                            + $"\n{thisScreenGroup.GetType().ToString()}"
+                            + $" (IMyTextPanel = {(thisScreenGroup is IMyTextPanel).ToString()})"
+                            + $"\nDefinition {((IMyTerminalBlock)thisScreenGroup).BlockDefinition.TypeIdString}/{((IMyTerminalBlock)thisScreenGroup).BlockDefinition.SubtypeIdAttribute}\n"
+                            + String.Join("\n", new string[] { "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19" })
+                        );
+                    }
+                }
+            }
+
+        } // Program() constructor
 
         public void Save()
         {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
+            this._theIniParser.Clear();
+            // ...
+            this.Storage = this._theIniParser.ToString();
         }
 
         public void Main(string argument, UpdateType updateSource)
         {
-            // The main entry point of the script, invoked every time
-            // one of the programmable block's Run actions are invoked,
-            // or the script updates itself. The updateSource argument
-            // describes where the update came from. Be aware that the
-            // updateSource is a  bitfield  and might contain more than 
-            // one update type.
-            // 
-            // The method itself is required, but the arguments above
-            // can be removed if not needed.
-        }
+            // This looks like an automatic call for the main loop.
+            if ((updateSource & (MAIN_LOOP_TYPE)) != 0)
+            {
+                // ...
+            }
+
+            // This is NOT an automatic call. Run it through the CLI.
+            if ((updateSource & (
+                UpdateType.Update1 | UpdateType.Update10 | UpdateType.Update100
+            )) == 0)
+            {
+                if (this._theCommandParser.TryParse(argument))
+                {
+                    Action commandAction;
+                    if (this._theCommandParser.ArgumentCount > 0)
+                    {
+                        string commandName = this._theCommandParser.Argument(0);
+                        if (null != commandName)
+                        {
+                            if (this._knownCommands.TryGetValue(commandName, out commandAction))
+                            {
+                                commandAction();
+                            }
+                            else
+                            {
+                                throw new Exception(
+                                    $"Command '{commandName}' does not exist."
+                                );
+                            }
+                        } // check and map command name
+                    } // if arguments existed
+                } // if could parse command line
+                else
+                {
+                    Echo("Program was run with no argument, from a source other than self-update. Doing nothing.");
+                }
+            }
+        } // Main()
     }
 }
---
+++ ScreenProfile.cs
@@ -0,0 +1,334 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class ScreenProfile
+        {
+            public enum SizeClasses
+            {
+                OneLine
+                , Tiny
+                , Small
+                , Medium
+                , Large
+            }
+
+            public SizeClasses SizeClass { get; private set; }
+
+            public float BestFontSize { get; private set; }
+
+            public int LineCount { get; private set; }
+
+            private ScreenProfile() { }
+
+            protected ScreenProfile(SizeClasses sizeClass, float bestFontSize, int lineCount) : this()
+            {
+                this.SizeClass = sizeClass;
+                this.BestFontSize = bestFontSize;
+                this.LineCount = lineCount;
+            }
+
+            public static ScreenProfile GetScreenProfile(IMyTextSurfaceProvider theBlock, int surfaceIndex)
+            {
+                return GetStandardScreenProfile(FindScreenType(theBlock, surfaceIndex));
+            }
+
+            public enum StandardScreens
+            {
+                ProgrammableLargeMain // Programmable block console screen
+                , ProgrammableLargeKeyboard // Programmable block "keyboard" screen
+                , CornerScreenLarge // One-line screen. Several mountings, same dimensions.
+                , LCDWideLarge // Double-wide big screen
+                , LCDLarge // 1x1 edge-to-edge screen
+                , TextPanelLarge // Smaller 1x1 screen
+                , ControlStationLarge // Seat with one large screen
+                , FlightSeatLarge // Seat with one low-profile screen
+                // Seat with desk and five screens
+                , ControlSeatLargeMain
+                , ControlSeatLargeLeftHand
+                , ControlSeatLargeLeftAux
+                , ControlSeatLargeRightBox
+                , ControlSeatLargeRightTTY
+                // Large-ship enclosed cockpit, industrial style with low-angle visibility. (Decorative Pack DLC)
+                , IndustrialLargeUpperMain // Far left corner, slightly larger
+                , IndustrialLargeUpperLeft
+                , IndustrialLargeUpperCenter
+                , IndustrialLargeUpperRight
+                , IndustrialLargeKeyboard // "Keyboard" screen between operator's hands
+                , IndustrialLargeNumpad // "Numpad" screen on the right arm rest. Barely visible.
+                // Large-ship enclosed cockpit, standard
+                , CockpitLargeUpperLeft
+                , CockpitLargeUpperCenter
+                , CockpitLargeUpperRight
+                , CockpitLargeKeyboard // Lower center, between pilot's hands
+                , CockpitLargeLowerLeft
+                , CockpitLargeLowerRight
+            }
+
+            private static IDictionary<StandardScreens, ScreenProfile> _standardScreenProfiles;
+            protected static ScreenProfile GetStandardScreenProfile(StandardScreens screenType)
+            {
+                if (null == _standardScreenProfiles)
+                {
+                    _standardScreenProfiles = new Dictionary<StandardScreens, ScreenProfile>
+                    {
+
+                        //TODO: Populate all known screen profiles
+                        {
+                            StandardScreens.ProgrammableLargeMain
+                            , new ScreenProfile(SizeClasses.Medium, 0.825f, 13)
+                        }
+                        ,
+                        {
+                            StandardScreens.ProgrammableLargeKeyboard
+                            , new ScreenProfile(SizeClasses.Small, 1.7f, 8)
+                        }
+                        ,
+                        {
+                            StandardScreens.CornerScreenLarge
+                            , new ScreenProfile(SizeClasses.OneLine, 9.0f, 1)
+                        }
+                        ,
+                        {
+                            StandardScreens.LCDWideLarge
+                            , new ScreenProfile(SizeClasses.Large, 1.575f, 11)
+                        }
+                        ,
+                        {
+                            StandardScreens.LCDLarge
+                            , new ScreenProfile(SizeClasses.Medium, 0.96f, 18)
+                        }
+                        ,
+                        {
+                            StandardScreens.TextPanelLarge
+                            , new ScreenProfile(SizeClasses.Medium, 1.025f, 10)
+                        }
+                        ,
+                        {
+                            StandardScreens.ControlStationLarge
+                            , new ScreenProfile(SizeClasses.Large, 0.607f, 17)
+                        }
+                        ,
+                        {
+                            StandardScreens.FlightSeatLarge
+                            , new ScreenProfile(SizeClasses.Small, 3.0f, 5)
+                        }
+                        ,
+                        {
+                            StandardScreens.ControlSeatLargeMain
+                            , new ScreenProfile(SizeClasses.Medium, 1.05f, 10)
+                        }
+                        ,
+                        {
+                            StandardScreens.ControlSeatLargeLeftHand
+                            , new ScreenProfile(SizeClasses.Small, 1.26f, 9)
+                        }
+                        ,
+                        {
+                            StandardScreens.ControlSeatLargeRightBox
+                            , new ScreenProfile(SizeClasses.Tiny, 1.9f, 6)
+                        }
+                        ,
+                        {
+                            StandardScreens.ControlSeatLargeLeftAux
+                            , new ScreenProfile(SizeClasses.Tiny, 3.5f, 5)
+                        }
+                        ,
+                        {
+                            StandardScreens.ControlSeatLargeRightTTY
+                            , new ScreenProfile(SizeClasses.Small, 1.45f, 9)
+                        }
+                        ,
+                        {
+                            StandardScreens.IndustrialLargeUpperMain
+                            , new ScreenProfile(SizeClasses.Small, 1.15f, 9)
+                        }
+                        ,
+                        {
+                            StandardScreens.IndustrialLargeUpperLeft
+                            , new ScreenProfile(SizeClasses.Medium, 1.2f, 10)
+                        }
+                        ,
+                        {
+                            StandardScreens.IndustrialLargeUpperCenter
+                            , new ScreenProfile(SizeClasses.Medium, 1.2f, 10)
+                        }
+                        ,
+                        {
+                            StandardScreens.IndustrialLargeUpperRight
+                            , new ScreenProfile(SizeClasses.Medium, 1.05f, 10)
+                        }
+                        ,
+                        {
+                            StandardScreens.IndustrialLargeKeyboard
+                            , new ScreenProfile(SizeClasses.Small, 1.3f, 8)
+                        }
+                        ,
+                        // This screen does exist, but it's practically impossible to see in the game.
+                        {
+                            StandardScreens.IndustrialLargeNumpad
+                            , new ScreenProfile(SizeClasses.Tiny, 1.0f, 1)
+                        }
+                        ,
+                        {
+                            StandardScreens.CockpitLargeUpperCenter
+                            , new ScreenProfile(SizeClasses.Medium, 1.08f, 11)
+                        }
+                        ,
+                        {
+                            StandardScreens.CockpitLargeUpperLeft
+                            , new ScreenProfile(SizeClasses.Medium, 1.17f, 11)
+                        }
+                        ,
+                        {
+                            StandardScreens.CockpitLargeUpperRight
+                            , new ScreenProfile(SizeClasses.Medium, 1.17f, 11)
+                        }
+                        ,
+                        {
+                            StandardScreens.CockpitLargeKeyboard
+                            , new ScreenProfile(SizeClasses.Small, 1.25f, 8)
+                        }
+                        ,
+                        {
+                            StandardScreens.CockpitLargeLowerLeft
+                            , new ScreenProfile(SizeClasses.Small, 1.65f, 8)
+                        }
+                        ,
+                        {
+                            StandardScreens.CockpitLargeLowerRight
+                            , new ScreenProfile(SizeClasses.Small, 1.65f, 8)
+                        }
+                    };
+                }
+
+                if (_standardScreenProfiles.ContainsKey(screenType))
+                {
+                    return _standardScreenProfiles[screenType];
+                }
+                else
+                {
+                    throw new ArgumentException(
+                        $"No ScreenProfile found for '{Enum.GetName(typeof(StandardScreens), screenType)}'. This is a bug."
+                        , "screenType"
+                    );
+                }
+            }
+
+            protected static StandardScreens FindScreenType(
+                IMyTextSurfaceProvider theBlock
+                , int surfaceIndex
+            )
+            {
+                if (null == theBlock)
+                {
+                    throw new ArgumentNullException("theBlock");
+                }
+                if (surfaceIndex < 0)
+                {
+                    throw new ArgumentException("surfaceIndex must be non-negative.", "surfaceIndex");
+                }
+                if (theBlock.SurfaceCount - 1 < surfaceIndex)
+                {
+                    throw new ArgumentException(
+                        $"Requested profile for surfaceIndex {surfaceIndex:n0}, but {((IMyTerminalBlock)theBlock).CustomName} only has {theBlock.SurfaceCount:n0} screen surface(s)."
+                        , "surfaceIndex"
+                    );
+                }
+
+                string exactType = ((IMyTerminalBlock)theBlock).BlockDefinition.TypeIdString + "/" + ((IMyTerminalBlock)theBlock).BlockDefinition.SubtypeIdAttribute;
+
+                switch (exactType)
+                {
+                    case "MyObjectBuilder_MyProgrammableBlock/LargeProgrammableBlock":
+                        if (0 == surfaceIndex)
+                            return StandardScreens.ProgrammableLargeMain;
+                        if (1 == surfaceIndex)
+                            // keyboard screen
+                            return StandardScreens.ProgrammableLargeKeyboard;
+                        break;
+                    case "MyObjectBuilder_TextPanel/LargeBlockCorner_LCD_Flat_1":
+                    case "MyObjectBuilder_TextPanel/LargeBlockCorner_LCD_Flat_2":
+                    case "MyObjectBuilder_TextPanel/LargeBlockCorner_LCD_1":
+                    case "MyObjectBuilder_TextPanel/LargeBlockCorner_LCD_2":
+                        // These skinny single-line screens have the same dimensions.
+                        return StandardScreens.CornerScreenLarge;
+                    case "MyObjectBuilder_TextPanel/LargeLCDPanelWide":
+                        return StandardScreens.LCDWideLarge;
+                    case "MyObjectBuilder_TextPanel/LargeLCDPanel":
+                        return StandardScreens.LCDLarge;
+                    case "MyObjectBuilder_TextPanel/LargeTextPanel":
+                        return StandardScreens.TextPanelLarge;
+                    case "MyObjectBuilder_Cockpit/LargeBlockCockpit":
+                        return StandardScreens.ControlStationLarge;
+                    case "MyObjectBuilder_Cockpit/CockpitOpen":
+                        return StandardScreens.FlightSeatLarge;
+                    case "MyObjectBuilder_Cockpit/OpenCockpitLarge":
+                        if (0 == surfaceIndex)
+                            return StandardScreens.ControlSeatLargeMain;
+                        if (1 == surfaceIndex)
+                            return StandardScreens.ControlSeatLargeLeftHand;
+                        if (2 == surfaceIndex)
+                            return StandardScreens.ControlSeatLargeRightBox;
+                        if (3 == surfaceIndex)
+                            return StandardScreens.ControlSeatLargeLeftAux;
+                        if (4 == surfaceIndex)
+                            return StandardScreens.ControlSeatLargeRightTTY;
+                        break;
+                    case "MyObjectBuilder_Cockpit/LargeBlockCockpitIndustrial":
+                        if (0 == surfaceIndex)
+                            return StandardScreens.IndustrialLargeUpperMain;
+                        if (1 == surfaceIndex)
+                            return StandardScreens.IndustrialLargeUpperLeft;
+                        if (2 == surfaceIndex)
+                            return StandardScreens.IndustrialLargeUpperCenter;
+                        if (3 == surfaceIndex)
+                            return StandardScreens.IndustrialLargeUpperRight;
+                        if (4 == surfaceIndex)
+                            return StandardScreens.IndustrialLargeKeyboard;
+                        if (5 == surfaceIndex)
+                            return StandardScreens.IndustrialLargeNumpad;
+                        break;
+                    case "MyObjectBuilder_Cockpit/LargeBlockCockpitSeat":
+                        if (0 == surfaceIndex)
+                            return StandardScreens.CockpitLargeUpperCenter;
+                        if (1 == surfaceIndex)
+                            return StandardScreens.CockpitLargeUpperLeft;
+                        if (2 == surfaceIndex)
+                            return StandardScreens.CockpitLargeUpperRight;
+                        if (3 == surfaceIndex)
+                            return StandardScreens.CockpitLargeKeyboard;
+                        if (4 == surfaceIndex)
+                            return StandardScreens.CockpitLargeLowerLeft;
+                        if (5 == surfaceIndex)
+                            return StandardScreens.CockpitLargeLowerRight;
+                        break;
+                }
+                throw new Exception(
+                    $"Screen type not known for block '{((IMyTerminalBlock)theBlock).CustomName}'. Is it non-vanilla?"
+                    + $"\nType Definition: '{exactType}'"
+                    + $"\nsurfaceIndex: {surfaceIndex:n0}"
+                );
+            }
+        }
+    }
+}
